ebc150201034e46c76db5bdab369cad476026ae1
Sumit Debroy <sroydeb2015@gmail.com>
2025-07-01 22:33:58 +0530

    ff

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 
-app.listen(3005, () => {
-  console.log("server started");
-});
+
 
 app.get("/", (req, res) => {
   res.render("anotherindex", { imagePath: null });
@@ -48,4 +46,4 @@ app.post("/upload", upload.single("image"), async (req, res) => {
 });
 
 
-// module.exports = app;
+module.exports = app;
